6e86a194e4906c72d5285a5105244243718380e1
Jonathan Ayala Zuñiga <jonathan_ayala2011@hotmail.com>
2023-08-25 17:29:56 -0500

    mas o menos el form de datos

--- View/HomeView.xaml.cs
+++ View/HomeView.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,38 @@ namespace SIRHU.View
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        private void txtCedula_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtCedula.Text == "Cédula")
+            {
+                txtCedula.Text = "";
+            }
+        }
+
+        private void txtCedula_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtCedula.Text == "")
+            {
+                txtCedula.Text = "Cédula";
+            }
+        }
+
+        private void txtNombre_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtNombre.Text == "Apellidos y Nombres")
+            {
+                txtNombre.Text = "";
+            }
+        }
+
+        private void txtNombre_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                txtNombre.Text = "Apellidos y Nombres";
+            }
         }
     }
 }
