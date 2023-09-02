406c033e18feed8db7816088289817aa77a9992a
Jonathan Ayala Zuñiga <jonathan_ayala2011@hotmail.com>
2023-09-02 14:12:36 -0500

    si

--- ViewModel/WorkersViewModel.cs
+++ ViewModel/WorkersViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using SIRHU.Models;
+using MaterialDesignThemes.Wpf;
 
 namespace SIRHU.ViewModel
 {
@@ -53,44 +54,377 @@ namespace SIRHU.ViewModel
         private IWorkerRepository workerRepository;
 
         //properties
-        public string Cedula { get => _cedula; set => _cedula = value; }
-        public int IdLocalidad { get => _idLocalidad; set => _idLocalidad = value; }
-        public int IdDepartamento { get => _idDepartamento; set => _idDepartamento = value; }
-        public string Nombres { get => _nombres; set => _nombres = value; }
-        public DateTime FechaIngreso { get => _fechaIngreso; set => _fechaIngreso = value; }
-        public string Cargo { get => _cargo; set => _cargo = value; }
-        public float SueldoBase { get => _sueldoBase; set => _sueldoBase = value; }
-        public bool PerDiscapacidad { get => _perDiscapacidad; set => _perDiscapacidad = value; }
-        public string Correo { get => _correo; set => _correo = value; }
-        public float HE25 { get => _hE25; set => _hE25 = value; }
-        public float HE50 { get => _hE50; set => _hE50 = value; }
-        public float HE100 { get => _hE100; set => _hE100 = value; }
-        public bool Estado { get => _estado; set => _estado = value; }
-        public DateTime FechaInactivo { get => _fechaInactivo; set => _fechaInactivo = value; }
-        public DateTime FechaReingreso { get => _fechaReingreso; set => _fechaReingreso = value; }
-        public float DT { get => _dT; set => _dT = value; }
-        public int PorcentajeDiscapacidad { get => _porcentajeDiscapacidad; set => _porcentajeDiscapacidad = value; }
-        public string TipoDiscapacidad { get => _tipoDiscapacidad; set => _tipoDiscapacidad = value; }
-        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
-        public string NumCelular { get => _numCelular; set => _numCelular = value; }
-        public string Direccion { get => _direccion; set => _direccion = value; }
-        public string ReferenciaDireccion { get => _referenciaDireccion; set => _referenciaDireccion = value; }
-        public string LatitudD { get => _latitudD; set => _latitudD = value; }
-        public string LongitudD { get => _longitudD; set => _longitudD = value; }
-        public string TelefonoConvencional { get => _telefonoConvencional; set => _telefonoConvencional = value; }
-        public string TelefonoCelular { get => _telefonoCelular; set => _telefonoCelular = value; }
-        public string Correo2 { get => _correo2; set => _correo2 = value; }
-        public string EstadoCivil { get => _estadoCivil; set => _estadoCivil = value; }
-        public string TipoRelacionLaboral { get => _tipoRelacionLaboral; set => _tipoRelacionLaboral = value; }
-        public string NovedadIngreso { get => _novedadIngreso; set => _novedadIngreso = value; }
-        public byte[] PdfIngreso { get => _pdfIngreso; set => _pdfIngreso = value; }
-        public int IdContrato { get => _idContrato; set => _idContrato = value; }
-        public int IdSectorial { get => _idSectorial; set => _idSectorial = value; }
-        public int IdCargas { get => _idCargas; set => _idCargas = value; }
-        public byte[] Foto { get => _foto; set => _foto = value; }
-        public string Nacionalidad { get => _nacionalidad; set => _nacionalidad = value; }
-        public string CargoFundasen { get => _cargoFundasen; set => _cargoFundasen = value; }
-        
+        public string Cedula
+        {
+            get { return _cedula; }
+            set
+            {
+                _cedula = value;
+                OnPropertyChanged(nameof(Cedula));
+            }
+        }
+
+        public int IdLocalidad
+        {
+            get { return _idLocalidad; }
+            set
+            {
+                _idLocalidad = value;
+                OnPropertyChanged(nameof(IdLocalidad));
+            }
+        }
+
+        public int IdDepartamento
+        {
+            get { return _idDepartamento; }
+            set
+            {
+                _idDepartamento = value;
+                OnPropertyChanged(nameof(IdDepartamento));
+            }
+        }
+
+        public string Nombres
+        {
+            get { return _nombres; }
+            set
+            {
+                _nombres = value;
+                OnPropertyChanged(nameof(Nombres));
+            }
+        }
+
+        public DateTime FechaIngreso
+        {
+            get { return _fechaIngreso; }
+            set
+            {
+                _fechaIngreso = value;
+                OnPropertyChanged(nameof(FechaIngreso));
+            }
+        }
+
+        public string Cargo
+        {
+            get { return _cargo; }
+            set
+            {
+                _cargo = value;
+                OnPropertyChanged(nameof(Cargo));
+            }
+        }
+
+        public float SueldoBase
+        {
+            get { return _sueldoBase; }
+            set
+            {
+                _sueldoBase = value;
+                OnPropertyChanged(nameof(SueldoBase));
+            }
+        }
+
+        public bool PerDiscapacidad
+        {
+            get { return _perDiscapacidad; }
+            set
+            {
+                _perDiscapacidad = value;
+                OnPropertyChanged(nameof(PerDiscapacidad));
+            }
+        }
+
+        public string Correo
+        {
+            get { return _correo; }
+            set
+            {
+                _correo = value;
+                OnPropertyChanged(nameof(Correo));
+            }
+        }
+
+        public float HE25
+        {
+            get { return _hE25; }
+            set
+            {
+                _hE25 = value;
+                OnPropertyChanged(nameof(HE25));
+            }
+        }
+
+        public float HE50
+        {
+            get { return _hE50; }
+            set
+            {
+                _hE50 = value;
+                OnPropertyChanged(nameof(HE50));
+            }
+        }
+
+        public float HE100
+        {
+            get { return _hE100; }
+            set
+            {
+                _hE100 = value;
+                OnPropertyChanged(nameof(HE100));
+            }
+        }
+
+        public bool Estado
+        {
+            get { return _estado; }
+            set
+            {
+                _estado = value;
+                OnPropertyChanged(nameof(Estado));
+            }
+        }
+
+        public DateTime FechaInactivo
+        {
+            get { return _fechaInactivo; }
+            set
+            {
+                _fechaInactivo = value;
+                OnPropertyChanged(nameof(FechaInactivo));
+            }
+        }
+
+        public DateTime FechaReingreso
+        {
+            get { return _fechaReingreso; }
+            set
+            {
+                _fechaReingreso = value;
+                OnPropertyChanged(nameof(FechaReingreso));
+            }
+        }
+
+        public float DT
+        {
+            get { return _dT; }
+            set
+            {
+                _dT = value;
+                OnPropertyChanged(nameof(DT));
+            }
+        }
+
+
+        public int PorcentajeDiscapacidad
+        {
+            get { return _porcentajeDiscapacidad; }
+            set
+            {
+                _porcentajeDiscapacidad = value;
+                OnPropertyChanged(nameof(PorcentajeDiscapacidad));
+            }
+        }
+
+        public string TipoDiscapacidad
+        {
+            get { return _tipoDiscapacidad; }
+            set
+            {
+                _tipoDiscapacidad = value;
+                OnPropertyChanged(nameof(TipoDiscapacidad));
+            }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set
+            {
+                _fechaNacimiento = value;
+                OnPropertyChanged(nameof(FechaNacimiento));
+            }
+        }
+
+        public string NumCelular
+        {
+            get { return _numCelular; }
+            set
+            {
+                _numCelular = value;
+                OnPropertyChanged(nameof(NumCelular));
+            }
+        }
+
+        public string Direccion
+        {
+            get { return _direccion; }
+            set
+            {
+                _direccion = value;
+                OnPropertyChanged(nameof(Direccion));
+            }
+        }
+
+        public string ReferenciaDireccion
+        {
+            get { return _referenciaDireccion; }
+            set
+            {
+                _referenciaDireccion = value;
+                OnPropertyChanged(nameof(ReferenciaDireccion));
+            }
+        }
+
+        public string LatitudD
+        {
+            get { return _latitudD; }
+            set
+            {
+                _latitudD = value;
+                OnPropertyChanged(nameof(LatitudD));
+            }
+        }
+
+        public string LongitudD
+        {
+            get { return _longitudD; }
+            set
+            {
+                _longitudD = value;
+                OnPropertyChanged(nameof(LongitudD));
+            }
+        }
+
+        public string TelefonoConvencional
+        {
+            get { return _telefonoConvencional; }
+            set
+            {
+                _telefonoConvencional = value;
+                OnPropertyChanged(nameof(TelefonoConvencional));
+            }
+        }
+
+        public string TelefonoCelular
+        {
+            get { return _telefonoCelular; }
+            set
+            {
+                _telefonoCelular = value;
+                OnPropertyChanged(nameof(TelefonoCelular));
+            }
+        }
+
+        public string Correo2
+        {
+            get { return _correo2; }
+            set
+            {
+                _correo2 = value;
+                OnPropertyChanged(nameof(Correo2));
+            }
+        }
+
+        public string EstadoCivil
+        {
+            get { return _estadoCivil; }
+            set
+            {
+                _estadoCivil = value;
+                OnPropertyChanged(nameof(EstadoCivil));
+            }
+        }
+
+        public string TipoRelacionLaboral
+        {
+            get { return _tipoRelacionLaboral; }
+            set
+            {
+                _tipoRelacionLaboral = value;
+                OnPropertyChanged(nameof(TipoRelacionLaboral));
+            }
+        }
+
+        public string NovedadIngreso
+        {
+            get { return _novedadIngreso; }
+            set
+            {
+                _novedadIngreso = value;
+                OnPropertyChanged(nameof(NovedadIngreso));
+            }
+        }
+
+        public byte[] PdfIngreso
+        {
+            get { return _pdfIngreso; }
+            set
+            {
+                _pdfIngreso = value;
+                OnPropertyChanged(nameof(PdfIngreso));
+            }
+        }
+
+        public int IdContrato
+        {
+            get { return _idContrato; }
+            set
+            {
+                _idContrato = value;
+                OnPropertyChanged(nameof(IdContrato));
+            }
+        }
+
+        public int IdSectorial
+        {
+            get { return _idSectorial; }
+            set
+            {
+                _idSectorial = value;
+                OnPropertyChanged(nameof(IdSectorial));
+            }
+        }
+
+        public int IdCargas
+        {
+            get { return _idCargas; }
+            set
+            {
+                _idCargas = value;
+                OnPropertyChanged(nameof(IdCargas));
+            }
+        }
+
+        public byte[] Foto
+        {
+            get { return _foto; }
+            set
+            {
+                _foto = value;
+                OnPropertyChanged(nameof(Foto));
+            }
+        }
+
+        public string Nacionalidad
+        {
+            get { return _nacionalidad; }
+            set
+            {
+                _nacionalidad = value;
+                OnPropertyChanged(nameof(Nacionalidad));
+            }
+        }
+
+        public string CargoFundasen
+        {
+            get { return _cargoFundasen; }
+            set
+            {
+                _cargoFundasen = value;
+                OnPropertyChanged(nameof(CargoFundasen));
+            }
+        }
+
         // Commands
 
         public ICommand AddWorker { get; }
@@ -122,7 +456,7 @@ namespace SIRHU.ViewModel
 
         private void ExecuteAddWorker(object obj)
         {
-            var isValidCedula = workerRepository.Add(new WorkerModel(Cedula, IdLocalidad,IdDepartamento, Nombres, FechaIngreso, fe ) );
+            workerRepository.Add(new WorkerModel()); ;
         }
 
         private bool CanExecuteAddWorker(object obj)
